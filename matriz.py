f4ec70aeab10ea63b05bf248947a5ff6ad46b54f
mateoyateg <mateoyateg@gmail.com>
2019-05-06 19:13:59 -0500

    Avances

--- matriz.py
+++ matriz.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+def cargarTexto():
+    return [x.split() for x in open("matriz.txt").readlines()]
 
 def mostrar(matriz):
 
@@ -8,15 +10,10 @@ def mostrar(matriz):
     if len(matriz[1:,:]) is not 0:
         mostrar(np.rot90(matriz[1:, :]))
 
-
-def cargarTexto():
-    return [x.split() for x in open("matriz.txt").readlines()]
-
-
 if __name__ == '__main__':
     print ("Matriz original:")
     print np.array(cargarTexto())
     print("")
 
-    print ("Matriz recorrida en forma de caracol: ")
+    print ("Matriz recorrida en forma de caracol:")
     mostrar(np.array(cargarTexto()))
